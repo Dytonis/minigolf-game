21dbbe48dcabde936e2b74eae8948063c94e81d8
Dytonis <dytonis123@gmail.com>
2017-03-26 19:22:18 -0600

    hole12, hole13, day/night integration start

--- An Amazing Golfing Adventure/Assets/Scripts/SunRotator.cs
+++ An Amazing Golfing Adventure/Assets/Scripts/SunRotator.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class SunRotator : MonoBehaviour
 {
+    public float limit;
+    public float speed;
+
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Rotate(new Vector3(0.005f, 0, 0));
+        transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
+
+        if(transform.eulerAngles.x >= limit)
+        {
+            enabled = false;
+        }
 	}
 }
