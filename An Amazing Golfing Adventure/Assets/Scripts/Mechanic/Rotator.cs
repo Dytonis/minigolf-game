765b347ae0e3f4d1c4520f182c443ed84deba7a5
Dytonis <dytonis123@gmail.com>
2017-03-28 02:23:43 -0600

    water mill

---
+++ An Amazing Golfing Adventure/Assets/Scripts/Mechanic/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    public bool X, Y, Z;
+    public float Speed;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    if(X)
+        {
+            transform.Rotate(new Vector3(Speed * Time.fixedDeltaTime, 0, 0));
+        }
+        if (Y)
+        {
+            transform.Rotate(new Vector3(0, Speed * Time.fixedDeltaTime, 0));
+        }
+        if (Z)
+        {
+            transform.Rotate(new Vector3(0, 0, Speed * Time.fixedDeltaTime));
+        }
+    }
+}
